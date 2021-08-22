02729b7835e17152149fdeda56f474b84b864e12
retore <44674058+retore404@users.noreply.github.com>
2021-08-22 17:55:53 +0900

    add: waitイベント

--- django/app/urls.py
+++ django/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('wait', views.wait_event, name='wait_event'),
 ]
--- django/app/views.py
+++ django/app/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+import time
+
 def index(request):
     return HttpResponse("Hello, world. You're at the apps index.")
+
+def wait_event(request):
+    time.sleep(1)
+    return HttpResponse("This page is displayed after 1 second wait.")
+
