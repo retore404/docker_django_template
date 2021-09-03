5507e5159c4fcdc2ecc98b9ad5b7ce2b56130814
retore <44674058+retore404@users.noreply.github.com>
2021-09-03 21:50:26 +0900

    delete: ベンチマーク用のwaitイベント削除

--- django/app/urls.py
+++ django/app/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('wait', views.wait_event, name='wait_event'),
 ]
--- django/app/views.py
+++ django/app/views.py
@@ -6,8 +6,3 @@ import time
 
 def index(request):
     return HttpResponse("Hello, world. You're at the apps index.")
-
-def wait_event(request):
-    time.sleep(0.5)
-    return HttpResponse("This page is displayed after 0.5 second wait.")
-
