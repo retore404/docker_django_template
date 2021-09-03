2096bad1f6da9c00660e94225ba8e441a80e40d4
retore <44674058+retore404@users.noreply.github.com>
2021-09-04 01:32:24 +0900

    add: ログインページ

--- django/app/urls.py
+++ django/app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = 'app'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name="app/login.html"), name='login'),
 ]
